ad6613aa6b2851effe7a88430f6c22fd5e874ab1
Anthony Fumagalli <anthony.fumagalli@gmail.com>
2016-06-29 13:00:20 -0700

    Implemented filter feature

--- js/app.js
+++ js/app.js
@@ -35,6 +35,8 @@ var map;
 
 var Location = function(data) {
 	this.name = ko.observable(data.name);
+	this.lat = ko.observable(data.lat);
+	this.lng = ko.observable(data.lng);
 	this.clicked = ko.observable(data.clicked);
 }
 
@@ -43,23 +45,22 @@ window.initMap = function(){
 		center: {lat: 37.759, lng: -122.423},
 		zoom: 16
 	});
-	dropMarkers();
 }
 
-function dropMarkers() {
+function dropMarkers(locations) {
 	clearMarkers();
-	for (var i = 0; i < initialLocations.length; i++) {
-		addMarkerWithTimeout(initialLocations[i], i * 200);
+	for (var i = 0; i < locations.length; i++) {
+		addMarkerWithTimeout(locations[i], 50);
 	}
 }
 
 function addMarkerWithTimeout(position, timeout) {
 	window.setTimeout(function() {
 		markers.push(new google.maps.Marker({
-			title: position.name,
-			position: {lat: position.lat, lng: position.lng},
+			title: position.name(),
+			position: {lat: position.lat(), lng: position.lng()},
 			map: map,
-			animation: google.maps.Animation.DROP
+			//animation: google.maps.Animation.DROP
 		}));
 	}, timeout);
 }
@@ -73,14 +74,28 @@ function clearMarkers() {
 
 var ViewModel = function() {
     var self = this;
+    this.query = ko.observable('')
     this.locationList = ko.observableArray([]);
     initialLocations.forEach(function(locationItem) {
         self.locationList.push(new Location(locationItem));
-    })
+    });
+    dropMarkers(this.locationList());
     this.currentLocation = ko.observable(this.locationList()[0]);
     this.setCurrentLocation = function(clickedLocation) {
         self.currentLocation(clickedLocation);
-    }
+    };
+    this.search = function(value) {
+		// remove all the current beers, which removes them from the view
+		self.locationList.removeAll();
+		for(var x in initialLocations) {
+			if(initialLocations[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+				self.locationList.push(new Location(initialLocations[x]));
+			}
+			dropMarkers(self.locationList());
+		}
+	};
+	this.query.subscribe(this.search);
 };
 
 ko.applyBindings(new ViewModel());
+//ViewModel.query.subscribe(ViewModel.search);
