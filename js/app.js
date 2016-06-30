08dea22dc57d6e039e4e0a3d88c7a7f596c6b1e5
Anthony Fumagalli <anthony.fumagalli@gmail.com>
2016-06-30 14:40:57 -0700

    Added InfoWindows to markers

--- js/app.js
+++ js/app.js
@@ -1,36 +1,38 @@
 var initialLocations = [
 	{
 		name: 'Dolores Park',
-		lat: 37.759,
-		lng: -122.428,
+		lat: 37.7597727,
+		lng: -122.427063,
 		clicked: false
 	},
 	{
 		name: 'Tartine Bakery',
-		lat: 37.761,
-		lng: -122.423,
+		lat: 37.7614184,
+		lng: -122.4241038,
 		clicked: false
 	},
 	{
 		name: 'Alamo Drafthouse Cinema',
-		lat: 37.756,
-		lng: -122.419,
+		lat: 37.7562163,
+		lng: -122.4191911,
 		clicked: false
 	},
 	{
 		name: 'Gracias Madre',
-		lat: 37.761,
-		lng: -122.419,
+		lat: 37.7615715,
+		lng: -122.4190799,
 		clicked: false
 	},
 	{
 		name: 'The Chapel',
-		lat: 37.761,
-		lng: -122.421,
+		lat: 37.760565,
+		lng: -122.421188,
 		clicked: false
 	}
 ];
+var contentString = "This is a test of the InfoWindow";
 var markers = [];
+var infoWindows = [];
 var map;
 
 var Location = function(data) {
@@ -49,12 +51,13 @@ window.initMap = function(){
 
 function dropMarkers(locations) {
 	clearMarkers();
+	console.log(locations);
 	for (var i = 0; i < locations.length; i++) {
-		addMarkerWithTimeout(locations[i], 50);
-	}
+		addMarkerWithTimeout(locations[i], 50, i);
+	};
 }
 
-function addMarkerWithTimeout(position, timeout) {
+function addMarkerWithTimeout(position, timeout, i) {
 	window.setTimeout(function() {
 		markers.push(new google.maps.Marker({
 			title: position.name(),
@@ -62,14 +65,23 @@ function addMarkerWithTimeout(position, timeout) {
 			map: map,
 			//animation: google.maps.Animation.DROP
 		}));
+		infoWindows.push(new google.maps.InfoWindow({
+			content: contentString
+		}));
+		console.log(markers[i]);
+		markers[i].addListener('click', function() {
+			infoWindows[i].open(map, markers[i]);
+		});
 	}, timeout);
 }
 
 function clearMarkers() {
 	for (var i = 0; i < markers.length; i++) {
+		google.maps.event.clearInstanceListeners(markers[i])
 		markers[i].setMap(null);
-	}
+	};
 	markers = [];
+	infoWindows = [];
 }
 
 var ViewModel = function() {
@@ -91,8 +103,8 @@ var ViewModel = function() {
 			if(initialLocations[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
 				self.locationList.push(new Location(initialLocations[x]));
 			}
-			dropMarkers(self.locationList());
 		}
+		dropMarkers(self.locationList());
 	};
 	this.query.subscribe(this.search);
 };
