b6b03ec3953db1ffa3798b4b83ebee23909eb610
Anthony Fumagalli <anthony.fumagalli@gmail.com>
2016-07-06 16:12:00 -0700

    Only allow one InfoWindow to be open at a time

--- js/app.js
+++ js/app.js
@@ -1,29 +1,36 @@
 var map;
+var infoWindow;
+// Initiazlie map and set up ViewModel
 initMap = function(){
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 37.759, lng: -122.423},
 		zoom: 16
 	});
+	// Use only one InfoWindow to keep max of one open at a time
+	infoWindow = new google.maps.InfoWindow({
+		content: null
+	});
 	ko.applyBindings(new ViewModel());
 };
 
+// Get information from NYTimes about each location
 var NYTimes = function(location) {
-	var nytElem = null;
 	var nytURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + location.name() + '&sort=newest&api-key=3ee99f8f736242c18f89225585db60a4';
 	$.getJSON(nytURL, function(data) {
 		var nytElem = ('<h3>New York Times Articles About ' + location.name() + '</h3><ul>');
 		articles = data.response.docs;
-		for (var i = 0; i < 1; i++) {
+		for (var i = 0; i < articles.length; i++) {
 			var article = articles[i];
 			nytElem += '<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>';;
 		};
-		location.infoWindow.setContent(nytElem);
+		infoWindow.setContent(nytElem);
 	}).fail(function(e) {
 		nytElem = 'New York Times Articles Could Not Be Loaded';
-		location.infoWindow.setContent(nytElem);
+		infoWindow.setContent(nytElem);
 	});
 };
 
+// Locations on the map
 var initialLocations = [
 	{
 		name: 'Dolores Park',
@@ -52,7 +59,8 @@ var initialLocations = [
 	}
 ];
 
-
+// Knockout model for locations
+// Also makes markers and InfoWindows
 var Location = function(data) {
 	var self = this;
 	this.name = ko.observable(data.name);
@@ -63,13 +71,11 @@ var Location = function(data) {
 		position: {lat: this.lat(), lng: this.lng()},
 		map: map
 	});
-	this.infoWindow = new google.maps.InfoWindow({
-		content: null
-	});
-	NYTimes(this);
+	// Get info from NYTimes and display in InfoWindow
 	this.showInfo = function() {
+		NYTimes(self);
 		self.marker.setAnimation(google.maps.Animation.BOUNCE);
-		self.infoWindow.open(map, self.marker);
+		infoWindow.open(map, self.marker);
 		setTimeout(function() {
 			self.marker.setAnimation(null);
 		}, 750);
@@ -79,8 +85,10 @@ var Location = function(data) {
 	});
 }
 
+// ViewModel for the application
 var ViewModel = function() {
 	var self = this;
+	// error handling
 	if (typeof google !== 'object' || typeof google.maps !== 'object') {
 		console.log("error loading Google Maps API");
 		return;
@@ -90,20 +98,24 @@ var ViewModel = function() {
 	initialLocations.forEach(function(locationItem) {
 		self.locationList.push(new Location(locationItem));
 	});
+	// Run the marker's showInfo function if list item clicked
 	this.showInfo = function() {
 		this.showInfo();
 	};
 	this.search = function(value) {
-		for(var x in self.locationList()) {
-			google.maps.event.clearInstanceListeners(self.locationList()[x].marker)
-			self.locationList()[x].marker.setMap(null);
+		// clear the map
+		for(var i in self.locationList()) {
+			google.maps.event.clearInstanceListeners(self.locationList()[i].marker)
+			self.locationList()[i].marker.setMap(null);
 		}
 		self.locationList.removeAll();
-		for(var x in initialLocations) {
-			if(initialLocations[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
-				self.locationList.push(new Location(initialLocations[x]));
+		// recreate list of locations based on search terms
+		for(var i in initialLocations) {
+			if(initialLocations[i].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+				self.locationList.push(new Location(initialLocations[i]));
 			}
 		}
 	};
+	// subscribe to updates to search
 	this.query.subscribe(this.search);
 };
