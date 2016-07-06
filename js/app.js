a0b3a0f8eee6b9d8b9f129bde49ed8793df1d0fa
afumagalli <anthony.fumagalli@gmail.com>
2016-07-05 22:35:40 -0700

    Moved InfoWindow content to model

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 var map;
-var contentString = "This is a test of the InfoWindow";
-window.initMap = function(){
+initMap = function(){
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat: 37.759, lng: -122.423},
 		zoom: 16
@@ -12,27 +11,32 @@ var initialLocations = [
 	{
 		name: 'Dolores Park',
 		lat: 37.7597727,
-		lng: -122.427063
+		lng: -122.427063,
+		content: "This is a test of the InfoWindow"
 	},
 	{
 		name: 'Tartine Bakery',
 		lat: 37.7614184,
-		lng: -122.4241038
+		lng: -122.4241038,
+		content: "This is a test of the InfoWindow"
 	},
 	{
 		name: 'Alamo Drafthouse Cinema',
 		lat: 37.7562163,
-		lng: -122.4191911
+		lng: -122.4191911,
+		content: "This is a test of the InfoWindow"
 	},
 	{
 		name: 'Gracias Madre',
 		lat: 37.7615715,
-		lng: -122.4190799
+		lng: -122.4190799,
+		content: "This is a test of the InfoWindow"
 	},
 	{
 		name: 'The Chapel',
 		lat: 37.760565,
-		lng: -122.421188
+		lng: -122.421188,
+		content: "This is a test of the InfoWindow"
 	}
 ];
 
@@ -41,20 +45,24 @@ var Location = function(data) {
 	this.name = ko.observable(data.name);
 	this.lat = ko.observable(data.lat);
 	this.lng = ko.observable(data.lng);
+	this.content = ko.observable(data.content);
 	this.marker = new google.maps.Marker({
 		title: this.name(),
 		position: {lat: this.lat(), lng: this.lng()},
 		map: map
 	});
 	this.infoWindow = new google.maps.InfoWindow({
-		content: contentString
+		content: this.content()
 	});
-	this.marker.addListener('click', function() {
+	this.showInfo = function() {
 		self.marker.setAnimation(google.maps.Animation.BOUNCE);
 		self.infoWindow.open(map, self.marker);
 		setTimeout(function() {
 			self.marker.setAnimation(null);
 		}, 750);
+	}
+	this.marker.addListener('click', function() {
+		self.showInfo();
 	});
 }
 
@@ -62,7 +70,6 @@ var ViewModel = function() {
 	var self = this;
 	this.query = ko.observable('')
 	this.locationList = ko.observableArray([]);
-	sessionStorage.clear();
 	if (typeof google !== 'object' || typeof google.maps !== 'object') {
 		console.log("error loading Google Maps API");
 		return;
@@ -70,8 +77,8 @@ var ViewModel = function() {
 	initialLocations.forEach(function(locationItem) {
 		self.locationList.push(new Location(locationItem));
 	});
-	this.bounceMarker = function() {
-
+	this.showInfo = function() {
+		this.showInfo();
 	};
 	this.currentLocation = ko.observable(this.locationList()[0]);
 	this.setCurrentLocation = function(clickedLocation) {
