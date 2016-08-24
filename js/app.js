11a9bc93cc75e1877015833829b1266b25f3c6d2
afumagalli <anthony.fumagalli@gmail.com>
2016-08-24 15:22:52 -0700

    Place markers based on geocoded address instead of hardcoded lat-lng

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 var map;
 var infoWindow;
+var geocoder;
 
 // Display an error message if map fails to load
 var googleMapError = function() {
@@ -14,6 +15,7 @@ var initMap = function(){
 		center: {lat: 37.759, lng: -122.423},
 		zoom: 16
 	});
+	geocoder = new google.maps.Geocoder();
 	// Use only one InfoWindow to keep max of one open at a time
 	infoWindow = new google.maps.InfoWindow({
 		content: null
@@ -47,50 +49,43 @@ var initMap = function(){
 var initialLocations = [
 	{
 		name: 'Taqueria Los Coyotes',
-		lat: 37.7597727,
-		lng: -122.427063,
+		address: '3036 16th St, San Francisco, CA 94103',
 		rating: 4,
 		review: 'This one has a special place in my heart for being the only taqueria in SF to have the California burrito - a San Diegan masterpiece of carne asada, pico de gallo, avocado, sour cream, and french fries. It’s not quite as good as the ones you’ll get down South, but it comes close. Unfortunately, it’s other San Diegan delicacy - carne asada fries - leaves much to be desired.'
 	},
 	{
 		name: 'Taqueria Cancun',
-		lat: 37.7614184,
-		lng: -122.4241038,
+		address: '2288 Mission St, San Francisco, CA 94110',
 		rating: 4,
 		review: 'A standard of the central Mission, Cancun can be hit or miss depending on the time of day and how many drunk people have invaded the space. The classic Mission-style burritos are excellent - and the size of a small child - but the quality can really drop late in the day. They’re open late, which is great when your 1:30 drunk munchies kick in but be warned: this place is cash only.'
 	},
 	{
 		name: 'Taqueria El Buen Sabor',
-		lat: 37.7562163,
-		lng: -122.4191911,
+		address: '699 Valencia St, San Francisco, CA 94110',
 		rating: 3,
 		review: 'As the main taqueria along the bougiest blocks of Valencia, El Buen Sabor sacrifices the guttural style of most other taquerias in favor of a decidedly less greasy smattering of options. The burritos are smaller (but just as pricey) as elsewhere and overall the food just isn’t as good as other nearby spots. But they do have a nice, lighter quesadilla and their breakfast burrito is excellent.'
 	},
 	{
 		name: 'Pancho Villa Taqueria',
-		lat: 37.7615715,
-		lng: -122.4190799,
+		address: '3071 16th St, San Francisco, CA 94103',
 		rating: 5,
 		review: 'Pancho Villa has managed to take everything that is great about the Mission burrito and expand on it. Their ingredients are incredibly fresh and meat wonderfully flavorful, but what really makes this the best spot in town is their unique technique of mixing your choice of salsa into your burrito. This is a beauty for spice-loving Mission residents who are tired of the district’s overall not-spicy-enough food.'
 	},
 	{
 		name: 'Taqueria El Farolito',
-		lat: 37.760565,
-		lng: -122.421188,
+		address: '2779 Mission St, San Francisco, CA 94110',
 		rating: 5,
 		review: ''
 	},
 	{
 		name: 'Taqueria El Toro',
-		lat: 0,
-		lng: 0,
+		address: '598 Valencia St, San Francisco, CA 94110',
 		rating: 3,
 		review: 'Don’t go to El Toro expecting the best burrito, but do go and get a plate of their excellent enchiladas. This is an excellent place to hit up when you get tired of the usual burrito fare (blasphemy, I know). '
 	},
 	{
 		name: 'La Taqueria',
-		lat: 0,
-		lng: 0,
+		address: '2889 Mission St, San Francisco, CA 94110',
 		rating: 5,
 		review: ''
 	}
@@ -100,18 +95,26 @@ var initialLocations = [
 // Also makes markers and InfoWindows
 var Location = function(data) {
 	var self = this;
-	this.name = ko.observable(data.name);
-	this.lat = data.lat;
-	this.lng = data.lng;
+	this.name = data.name;
+	this.address = data.address;
 	this.rating = data.rating;
 	this.review = data.review;
 	this.visible = ko.observable(true);
-	this.marker = new google.maps.Marker({
-		title: this.name(),
-		position: {lat: this.lat, lng: this.lng},
-		map: map
-	});
-	// Get info from NYTimes and display in InfoWindow
+	geocoder.geocode({'address': this.address}, function(results, status) {
+		if (status == 'OK') {
+			self.marker = new google.maps.Marker({
+				title: self.name,
+				position: results[0].geometry.location,
+				map: map
+			});
+			self.marker.addListener('click', function() {
+				self.showInfo();
+			});
+		} else {
+			alert('Geocode was no successful for the following reason: ' + status);
+		}
+	})
+	// Display rating and review in info window
 	this.showInfo = function() {
 		var content = '<div class="rating">';
 		for (var i = 0; i < this.rating; i++) {
@@ -125,9 +128,6 @@ var Location = function(data) {
 			self.marker.setAnimation(null);
 		}, 700);
 	};
-	this.marker.addListener('click', function() {
-		self.showInfo();
-	});
 };
 
 // ViewModel for the application
@@ -150,7 +150,7 @@ var ViewModel = function() {
 	this.search = function(value) {
 		// only locations that match search criteria should be visible
 		self.locationList().forEach(function(location) {
-			if(location.name().toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+			if(location.name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
 				location.marker.setVisible(true);
 				location.visible(true);
 			}
