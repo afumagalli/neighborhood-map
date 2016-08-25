2aced52ea16d5d00560d875ddb3f52b6fa8281af
afumagalli <anthony.fumagalli@gmail.com>
2016-08-24 17:11:51 -0700

    Update layout and style

--- js/app.js
+++ js/app.js
@@ -145,7 +145,12 @@ var initialLocations = [
 	},
 ];
 initialLocations.sort(function(a, b) {
-	return a.name < b.name;
+	var nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase();
+	if (nameA < nameB)
+		return -1;
+	if (nameA > nameB)
+		return 1;
+	return 0;
 });
 
 // Knockout model for locations
